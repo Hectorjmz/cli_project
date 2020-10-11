6f964e575db1986f0dae55ceadba8639f0c5b270
Hector_Jimenez <hjmontemayor@gmail.com>
2020-10-11 12:20:55 -0400

    add random quote about design driver

--- lib/cli.rb
+++ lib/cli.rb
@@ -21,23 +21,27 @@ class CLI
             input = gets.strip
             if input == 'exit'
                 break
-            else
+            elsif (input.to_i > 0 && input.to_i <= Breweries.all.length)
                 input = input.to_i
-                if input > 0 
-                    print_info_store(input)
-                end
+                    if input > 0 
+                        print_info_store(input)
+                    end
+            else
+                puts "That input is not avaliable"
             end 
             Breweries.clear    
             puts "Press any key to continue searching (type 'exit' to exit)"
             input = gets.strip.downcase
         end
+        puts ""
         goodbye
+        puts ""
 
     end
 
     def main_menu
         puts ""
-        puts "WELCOME TO BREWERIY FINDER"
+        puts "WELCOME TO BREWERIY FINDER.."
         puts ""
         puts "This program help you to find Breweries based in location"
         puts "Type any key to continue (type 'exit' to exit)"
@@ -68,7 +72,34 @@ class CLI
     end
 
     def goodbye
-        puts "Goodbye.....maybe some funnt quotes"
+        quotes_array = [
+            "“A designated driver helps you party another day.”",
+            "“A tree never hits an automobile except in self defense.” » American Proverb",
+            "“Allow Life To Thrive, Don’t Drink And Drive.” Slogansmotto",
+            "“Automobiles are not ferocious…. it is man who is to be feared. ” » Robbins B. Stoeckel",
+            "“Been To The Bash? Please Don’t Crash!”",
+            "“Better To Arrive Sober And Late Than Never.”",
+            "“Buzzed driving is drunk driving.”",
+            "“Don’t Drink And Drive, And You’ll Stay Alive.”",
+            "“Drink and drive, the police will show you some new bars.”",
+            "“Drink+Drive = Deadly Cocktail”",
+            "“Drink, Drunk, Drive, Dead”",
+            "“Drinking + Driving = Disaster”",
+            "“Drinking And Driving Is A Gamble That You Just Can’t Win.”",
+            "“Drinking And Driving – There Are Stupider Things, But It’S A Very Short List”",
+            "“Drive hammered, get slammered.”",
+            "“Driving Hammered Will Get You Nailed”",
+            "“Drunk driving is a killer disease.”",
+            "“Friends Don’t Let Friends Drive Drunk.”",
+            "“Honk if you love Jesus. Text while driving if you want to meet him.”, as seen on a church sign",
+            "“If Partying Is A Must, Designate A Driver That You Trust.”",
+            "“If You Drink And Drive, You’re Going To Make Someone Cry.”",
+            "“If you drink, don’t drive. Don’t even putt.” » Dean Martin",
+            "“If you must drink and drive, drink Pepsi.” » Bumper sticker"
+        ]
+
+        puts quotes_array.sample
+        
     end
 
 end
